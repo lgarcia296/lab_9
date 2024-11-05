4337fc0934e0a5f5358cf5697e6f2b93aa268340
Leonel Garcia <lgarcia2961@gmail.com>
2024-11-05 15:13:02 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function init(){
+var button = document.getElementById('entrybutton');
+
+function showMeText(){
+	var textbox = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML =textbox.value;
+	alert("Leonel Garcia: " + textbox.value);
+	}
+	
+	button.addEventListener('click', showMeText);
+	}
+	
+	window.addEventListener('load', init);
+ 
+
+
+
+
+
+
+
+window.addEventListener('load', init);
